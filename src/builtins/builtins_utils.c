0d1a7a84207e1c52137f583f3271585c4df5f31a
fivieira <oreo.sousa@hotmail.com>
2024-07-07 11:33:03 +0100

    env implement, need test and need return with signals

---
+++ src/builtins/builtins_utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fivieira <fivieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 10:56:02 by fivieira          #+#    #+#             */
+/*   Updated: 2024/07/07 11:20:18 by fivieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	prtstr_env(char **array)
+{
+	int i;
+
+	if(array == NULL)
+		return ;
+	i = -1;
+	while(array[++i] != NULL)
+	{
+		if(ft_strchr(array[i], '='))
+			ft_printf("%s\n", array[i]);
+	}	
+}	
